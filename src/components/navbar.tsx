e985ade6b1b2f972629defec5169063f8bda1ffb
ali <Alikhaydarov01@gmail.com>
2024-03-29 17:21:32 +0500

    update eslink file

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -74,9 +74,11 @@ const Navbar = () => {
                         <li>
                             <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
                         </li>
+                        <Link href={'/home'} locale='en'>
                         <li>
                             <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                         </li>
+                        </Link>
                     </ul>
                 </div>
             </div>
