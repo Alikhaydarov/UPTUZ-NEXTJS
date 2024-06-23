42070e339364ea36f7b56791b7c0a84a734161dc
ali <Alikhaydarov01@gmail.com>
2024-06-23 14:01:40 +0500

    update

--- src/app/[locale]/team/page.tsx
+++ src/app/[locale]/team/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { FaTelegram } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
 function page() {
   return (
     <section className="text-gray-600 body-font">
-    <div className="container px-5 py-10 mx-auto">
+    <div className="container max-sm:items-center px-36 max-sm:px-5  py-10 mx-auto">
       <div className="flex flex-col text-center w-full mb-10">
         <h1 className="text-2xl font-medium title-font  text-gray-900 dark:text-white">OUR TEAM</h1>
         {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p> */}
@@ -11,26 +13,20 @@ function page() {
       <div className="flex flex-wrap -m-4">
         <div className="p-4 lg:w-1/4 md:w-1/2">
           <div className="h-full flex flex-col items-center text-center">
-            <img alt="team" className="flex-shrink-0 rounded-lg w-50% h-[350px] object-cover object-center mb-4" src="https://uptuz.vercel.app/assets/20240209_152948-Fm14QjzX.jpg"/>
+            <img alt="team" className="flex-shrink-0 rounded-lg w-50% h-[350px]  max-sm:w-[500px] max-sm:h-[80%] object-cover object-center mb-4" src="https://uptuz.vercel.app/assets/20240209_152948-Fm14QjzX.jpg"/>
             <div className="w-full">
               <h2 className="itle-font font-medium text-lg text-gray-900  dark:text-white">Ali Khaydarov</h2>
               <h3 className="text-gray-500 mb-3">FrontEnd Developer</h3>
               <p className="mb-4"></p>
               <span className="inline-flex">
-                <a className="text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
+                <a className="text-gray-500" href='https://t.me/tolibovich_17'>
+                 <FaTelegram  style={{fontSize: "20px"}}/>
                 </a>
-                <a className="ml-2 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
+                <a className="ml-2 text-gray-500" href='https://www.instagram.com/khaydaroff.dev/'>
+                <FaInstagram style={{fontSize: "20px"}} />
                 </a>
-                <a className="ml-2 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
+                <a className="ml-2 text-gray-500" href='https://github.com/Alikhaydarov/'>
+                <FaGithub style={{fontSize: "20px"}}  />
                 </a>
               </span>
             </div>
@@ -38,26 +34,20 @@ function page() {
         </div>
         <div className="p-4 lg:w-1/4 md:w-1/2">
           <div className="h-full flex flex-col items-center text-center">
-            <img alt="team" className="flex-shrink-0 rounded-lg w-50% h-[350px] object-cover object-center mb-4" src="https://uptuz.vercel.app/assets/photo_3_2024-01-12_23-18-57-14G6P4QJ.jpg"/>
+            <img alt="team" className="flex-shrink-0 rounded-lg w-50% h-[350px]  max-sm:w-[500px] max-sm:h-[80%] object-cover object-center mb-4" src="https://uptuz.vercel.app/assets/photo_3_2024-01-12_23-18-57-14G6P4QJ.jpg"/>
             <div className="w-full">
               <h2 className="itle-font font-medium text-lg text-gray-900  dark:text-white">Ulug'bek Ismoilov</h2>
               <h3 className="text-gray-500 mb-3">BackEnd Developer</h3>
               {/* <p className="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p> */}
               <span className="inline-flex">
-                <a className="text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
+                <a className="text-gray-500" href='https://t.me/uptuz_owner'>
+                 <FaTelegram  style={{fontSize: "20px"}}/>
                 </a>
-                <a className="ml-2 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
+                <a className="ml-2 text-gray-500" href='https://www.instagram.com/samur0770/'>
+                <FaInstagram style={{fontSize: "20px"}} />
                 </a>
-                <a className="ml-2 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
+                <a className="ml-2 text-gray-500" href='https://github.com/UPTUZOVER'>
+                <FaGithub style={{fontSize: "20px"}}  />
                 </a>
               </span>
             </div>
@@ -65,26 +55,20 @@ function page() {
         </div>
         <div className="p-4 lg:w-1/4 md:w-1/2">
           <div className="h-full flex flex-col items-center text-center">
-            <img alt="team" className="flex-shrink-0 rounded-lg w-50% h-[350px] object-cover object-center mb-4" src="	https://uptuz.vercel.app/assets/photo_2_2024-01-12_23-18-57-CU9kBL5X.jpg"/>
+            <img alt="team" className="flex-shrink-0 rounded-lg w-50% h-[350px]  max-sm:w-[500px] max-sm:h-[80%] object-cover object-center mb-4" src="	https://uptuz.vercel.app/assets/photo_2_2024-01-12_23-18-57-CU9kBL5X.jpg"/>
             <div className="w-full">
               <h2 className="itle-font font-medium text-lg text-gray-900  dark:text-white">Orzubek Ashurqulov</h2>
               <h3 className="text-gray-500 mb-3">Web Developer</h3>
               {/* <p className="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p> */}
               <span className="inline-flex">
-                <a className="text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
+                <a className="text-gray-500" href=''>
+                 <FaTelegram  style={{fontSize: "20px"}}/>
                 </a>
-                <a className="ml-2 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
+                <a className="ml-2 text-gray-500" href=''>
+                <FaInstagram style={{fontSize: "20px"}} />
                 </a>
-                <a className="ml-2 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
+                <a className="ml-2 text-gray-500" href=''>
+                <FaGithub style={{fontSize: "20px"}}  />
                 </a>
               </span>
             </div>
@@ -92,26 +76,20 @@ function page() {
         </div>
         <div className="p-4 lg:w-1/4 md:w-1/2">
           <div className="h-full flex flex-col items-center text-center">
-            <img alt="team" className="flex-shrink-0 rounded-lg w-50% h-[350px] object-cover object-center mb-4" src="https://uptuz.vercel.app/assets/photo_2024-01-17_19-12-49-qrgV3VDU.jpg"/>
+            <img alt="team" className="flex-shrink-0 rounded-lg w-50% h-[350px]  max-sm:w-[500px] max-sm:h-[80%] object-cover object-center mb-4" src="https://uptuz.vercel.app/assets/photo_2024-01-17_19-12-49-qrgV3VDU.jpg"/>
             <div className="w-full">
               <h2 className="itle-font font-medium text-lg text-gray-900  dark:text-white">Og'abek</h2>
               <h3 className="text-gray-500 mb-3">Designer</h3>
               {/* <p className="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p> */}
               <span className="inline-flex">
-                <a className="text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
+                <a className="text-gray-500" href=''>
+                 <FaTelegram  style={{fontSize: "20px"}}/>
                 </a>
-                <a className="ml-2 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
+                <a className="ml-2 text-gray-500" href=''>
+                <FaInstagram style={{fontSize: "20px"}} />
                 </a>
-                <a className="ml-2 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
+                <a className="ml-2 text-gray-500" href=''>
+                <FaGithub style={{fontSize: "20px"}}  />
                 </a>
               </span>
             </div>
