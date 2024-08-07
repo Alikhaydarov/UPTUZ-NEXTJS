c27aa4b097eb6cd1134e4257880c3736392f7b2e
Ali <alikhaydarov01@gmail.com>
2024-08-07 18:42:25 +0500

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,20 +1,24 @@
 import { useTranslations } from 'next-intl'
-import Hero from '../../components/hero'
-import Services from './services/page'
-import Team from './team/page'
-import Header from '../../components/header'
+import { ThemeProvider } from 'next-themes'
 import Footer from '../../components/footer'
+import Header from '../../components/header'
+import Hero from '../../components/hero'
 import TeamCarousel from '../../components/TeamCarousel'
-
+import Services from './services/page'
 
 // import Services  from '@/components/services';
 export default function Home() {
-  const t = useTranslations('Index')
-  return (<div className=''><Header/>
-  <Hero/>
-  <Services/>
-  <TeamCarousel/>
-  {/* <Team/> */}
-  <Footer/>
-  </div>)
+	const t = useTranslations('Index')
+	return (
+		<ThemeProvider attribute='class'>
+			<div className=''>
+				<Header />
+				<Hero />
+				<Services />
+				<TeamCarousel />
+				{/* <Team/> */}
+				<Footer />
+			</div>
+		</ThemeProvider>
+	)
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,37 +1,38 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
+import { FaTelegramPlane, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
 import logo from './../../public/photo_2024-01-02_20-00-52__2_-removebg-preview (1).png'
 import logo2 from './../../public/photo_2024-01-02_20-08-51-removebg-preview (1).png'
-import Image from 'next/image'
-import { FaTelegramPlane } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
+    const { theme } = useTheme()
+
     return (
-        <>
-            <footer className="text-gray-800">
-                <div className="container px-36 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                <Link href={"/"}>
-                    <Image src={logo} alt='logo' className='' height={"45"} />
+        <footer className="text-gray-800 bg-white dark:bg-gray-900">
+            <div className="container mx-auto lg:px-16 py-8 flex flex-col sm:flex-row items-start">
+                <Link href="/" className="mb-4 sm:mb-0 sm:mr-4">
+                    <Image src={theme === 'dark' ? logo2 : logo} alt='logo' height={45} />
                 </Link>
-                 <p className="text-sm text-black dark:text-gray-300 font-semibold sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 dark">© 2024 UPTUZ — Barcha huquqlari himoyalangan.</p>
-                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                        <a href='https://t.me/uptuz' className="dark:text-gray-300 flex cursor-pointer text-black">
-                        <FaTelegramPlane className="text-2xl "/>  <span className="px-3">Telegram</span>
-                        </a>
-                        <a className="text-black dark:text-gray-300 flex cursor-pointer">
-                        <FaYoutube  className="text-2xl "/>  <span className="px-3">Youtube</span>
-                        </a>
-                        <a  href="https://www.instagram.com/uptuz" className="text-black dark:text-gray-300 flex cursor-pointer">
-                        <FaInstagram className="text-2xl "/>  <span className="px-3">Instagram</span>
-                        </a>
-                    </span>
-                </div>
-            </footer>
-        </>
+                <p className="text-sm text-black dark:text-gray-300 font-semibold sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 dark:sm:border-gray-700 sm:py-2 sm:mt-0 mt-4">
+                    © 2024 UPTUZ — Barcha huquqlari himoyalangan.
+                </p>
+                <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                    <a href='https://t.me/uptuz' className="dark:text-gray-300 flex cursor-pointer text-black items-center mr-4">
+                        <FaTelegramPlane className="text-2xl" /> <span className="px-2">Telegram</span>
+                    </a>
+                    <a className="text-black dark:text-gray-300 flex cursor-pointer items-center mr-4">
+                        <FaYoutube className="text-2xl" /> <span className="px-2">Youtube</span>
+                    </a>
+                    <a href="https://www.instagram.com/uptuz" className="text-black dark:text-gray-300 flex cursor-pointer items-center">
+                        <FaInstagram className="text-2xl" /> <span className="px-2">Instagram</span>
+                    </a>
+                </span>
+            </div>
+        </footer>
     )
 }
 
-export default Footer
+export default Footer;
