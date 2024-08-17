a2c939e82f3cfdba3926341ab623c17b056aef97
Ali Khaydarov <141232212+Alikhaydarov@users.noreply.github.com>
2024-08-17 15:45:00 +0500

    Update ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -15,7 +15,7 @@ export default function ThemeToggle() {
                 setTheme(resolvedTheme === "light" ? "dark" : "light");
             }}
             type='button'
-            className='rounded-3xl lg:mr-5 p-3 bg-violet-800 text-slate-100 dark:bg-violet-800'>
+            className='rounded-3xl lg:mr-7 p-3 bg-violet-800 text-slate-100 dark:bg-violet-800'>
             {theme === "light" ? <span className='text-lg'><svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
             </svg></span> : <span className='text-xl'><svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
